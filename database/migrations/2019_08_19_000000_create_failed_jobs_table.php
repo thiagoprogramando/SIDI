1c1502f666c968501b6b857f759bbc8df04fc960
Thiago César <66066163+thiagoprogramando@users.noreply.github.com>
2024-09-24 10:06:44 -0300

    Ajuste nos migrations: corrigido relacionamento entre tabelas

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -4,13 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
+return new class extends Migration {
+
+    public function up(): void {
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
             $table->string('uuid')->unique();
@@ -22,11 +18,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('failed_jobs');
     }
 };
--- database/migrations/2024_09_23_155146_create_price_table.php
+++ database/migrations/2024_09_23_155146_create_price_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
         Schema::create('price', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('product_id');
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('product')->onDelete('cascade');
             $table->unsignedBigInteger('client_id');
             $table->foreign('client_id')->references('id')->on('users')->onDelete('cascade');
             $table->decimal('value', 10, 2)->default(0);
