8be2157878f9eee3aabbabd33f813316390b770d
Thiago César <66066163+thiagoprogramando@users.noreply.github.com>
2024-09-24 10:19:14 -0300

    Configurando rota inicial para login

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/login', [LoginController::class, 'login'])->name('login');
